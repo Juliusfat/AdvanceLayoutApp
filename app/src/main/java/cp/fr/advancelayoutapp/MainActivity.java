6adfb28cf51f13ab05016c39c0af3a9488f4d84e
Cyrille Paillard <cyrille.paillard@free.fr>
2018-01-15 16:07:39 +0100

    ajout de fragment

--- app/src/main/java/cp/fr/advancelayoutapp/MainActivity.java
+++ app/src/main/java/cp/fr/advancelayoutapp/MainActivity.java
@@ -1,7 +1,10 @@
 package cp.fr.advancelayoutapp;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * ajout d'un fragment
+     * @param view
+     */
+
+    public void onAddFragment(View view) {
+        //instancier un fragment
+        FragmentB fragmentB = new FragmentB();
+        //recuperer une instance du getionnaire de fragments
+        FragmentManager manager = getFragmentManager();
+        //début de la transaction
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.fragmentContainer,fragmentB);
+        transaction.commit();
+
     }
 }
